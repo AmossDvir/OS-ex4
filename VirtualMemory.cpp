13c8c81f61cca8c6a4eb0ad8587fbf481a39faf4
AmossDvir1 <dvir.amoss@gmail.com>
2023-06-17 23:58:23 +0300

    implemented a basic translation from virtual to physical
    added pseudo codes for the VMread and VMwrite functions

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -2,16 +2,73 @@
 // Created by user on 6/12/2023.
 //
 
+#include <iostream>
 #include "VirtualMemory.h"
 
 #define SUCCESS 1
 #define FAILURE 0
+
+const uint64_t offsetMask = (1ULL << OFFSET_WIDTH) - 1;
+const uint64_t pageMask = ~offsetMask;
+
+/**
+ * Extracts the page number from the virtual address
+ * @param virtualAddress
+ * @return
+ */
+uint64_t extractPageNumber(uint64_t virtualAddress)
+{
+    return virtualAddress & pageMask;
+}
+
+/**
+ * Extracts the offset from the virtual address
+ * @param virtualAddress
+ * @return
+ */
+uint64_t extractOffset(uint64_t virtualAddress)
+{
+    return virtualAddress & offsetMask;
+}
+
+/**
+ * Validates the page number
+ * @param pageNum
+ * @return
+ */
+bool validatePageNumber(uint64_t pageNum)
+{
+    return (pageNum < VIRTUAL_MEMORY_SIZE);
+}
+
+/**
+ * Translates virtual address to a physical address
+ * @param virtualAddress
+ * @param physical_address
+ * @return
+ */
+int translateLogicToPhysical(uint64_t virtualAddress, uint32_t *physical_address)
+{
+    uint64_t offset = extractOffset(virtualAddress);
+    uint64_t pageNum = extractPageNumber(virtualAddress);
+    if (!validatePageNumber(pageNum))
+    {
+        return FAILURE;
+    }
+
+    //go to the page tables
+    uint64_t frameNum = 0; //pages table[pageNum]
+    *physical_address = (frameNum << OFFSET_WIDTH) | offset;
+    return SUCCESS;
+}
+
+
 /*
  * Initialize the virtual memory.
  */
-void VMinitialize ()
+void VMinitialize()
 {
-  PMwrite (0, 0);
+    PMwrite(0, 0);
 }
 
 /* Reads a word from the given virtual address
@@ -21,12 +78,20 @@ void VMinitialize ()
  * returns 0 on failure (if the address cannot be mapped to a physical
  * address for any reason)
  */
-int VMread (uint64_t virtualAddress, word_t *value)
+int VMread(uint64_t virtualAddress, word_t *value)
 {
-//    if (virtualAddress > || virtualAddress < ){// todo: figure out when to return error
-//    return FAILURE;
-//    }
-  return SUCCESS;
+    uint32_t physical_address;
+    int ret = translateLogicToPhysical(virtualAddress, &physical_address);
+
+    // If the translation failed, then do nothing.
+    if (ret == FAILURE)
+    {
+        return FAILURE;
+    }
+
+    // Read the value from the physical address.
+    PMread(physical_address, value);
+    return SUCCESS;
 }
 
 /* Writes a word to the given virtual address.
@@ -35,88 +100,74 @@ int VMread (uint64_t virtualAddress, word_t *value)
  * returns 0 on failure (if the address cannot be mapped to a physical
  * address for any reason)
  */
-int VMwrite (uint64_t virtualAddress, word_t value)
+int VMwrite(uint64_t virtualAddress, word_t value)
 {
-//    if (virtualAddress > || virtualAddress < ){// todo: figure out when to return error
-//        return FAILURE;
-//    }
-  return SUCCESS;
+    uint32_t physical_address;
+    int ret = translateLogicToPhysical(virtualAddress, &physical_address);
+    if (ret == FAILURE)
+    {
+        return FAILURE;
+    }
+
+    PMwrite(physical_address, value);
+    return SUCCESS;
 }
 
-/****/
-uint64_t translateLogicToPhysical (uint64_t virtualAddress)
-{
-  const uint64_t offsetMask = (1ULL << OFFSET_WIDTH) - 1;
-  uint64_t offset = virtualAddress & offsetMask;
-  const uint64_t pageMask = ~offsetMask;
-  uint64_t pageNum = virtualAddress & pageMask;
 
-  //go to the page tables
-  uint64_t frameNum = 0;//pages table[pageNum]
-  uint64_t physicalAddress = (frameNum << OFFSET_WIDTH) | offset;
-  return physicalAddress;
+bool isPageInPhysical(uint64_t pageNum)
+{
+    return false;//page table [page][isValid]
 }
 
-bool isPageInPhysical (uint64_t pageNum)
+void moveToHardDisk()
 {
-  return false;//page table [page][isValid]
 }
-
-void moveToHardDisk ()
-{}
 
 //layersNum=TABLES_DEPTH
-void tryFunc (uint64_t virtualAddress,word_t value)
+void tryFunc(uint64_t virtualAddress, word_t value)
 {
-  uint64_t frameNum = 0;
-  uint64_t tempOffset = 0;
+    uint64_t frameNum = 0;
+    uint64_t tempOffset = 0;
 
-  const uint64_t offsetMask = (1ULL << OFFSET_WIDTH) - 1;
-  uint64_t offset = virtualAddress & offsetMask;//1
-  const uint64_t pageMask = ~offsetMask;
-  uint64_t pageNum = virtualAddress & pageMask;//0110(6)
+    uint64_t offset = virtualAddress & offsetMask;//1
+    uint64_t pageNum = virtualAddress & pageMask;//0110(6)
 
-//  uint64_t unusedFrame=0;
-  uint64_t sum = 0;
-  uint64_t physicalAddress=0;
-  for (int i = 0; i < TABLES_DEPTH; i++)
+    //  uint64_t unusedFrame=0;
+    uint64_t sum = 0;
+    uint64_t physicalAddress = 0;
+    for (int i = 0; i < TABLES_DEPTH; i++)
     {
-//      PMread(0 + 5, &addr1) // first translation
-//      If (addr1 == 0) then
-//      Find an unused frame or evict a page from some frame. Suppose this frame number is f1
-//      Write 0 in all of its contents (only necessary if next layer is a table)
-//      PMwrite(0 + 5, f1)
-//      addr1 = f1
-//      PMread(addr1 * PAGE_SIZE + 1, &addr2) // second translation
-//      If (addr2 == 0) then
-//      Find an unused frame or evict a page from some frame. Suppose this frame number is f2.
-//      Make sure you are not evicting f1 by accident
-//      Restore the page we are looking for to frame f2 (only necessary pointing to a page)
-//      PMwrite(addr1 * PAGE_SIZE + 1, f2)
-//      addr2 = f2
-//      PMwrite(addr2 * PAGE_SIZE + 6, value)
-      //where to use offset?
-      uint64_t currentBit = (pageNum >> i) & 1;
-      sum += currentBit;
-      word_t tempValue = 0;//
-      //sholud check both options of the offset? (or even general number of
-      // the offset)
-      PMread (sum, &tempValue);
-      if (tempValue != 0)
+        //      PMread(0 + 5, &addr1) // first translation
+        //      If (addr1 == 0) then
+        //      Find an unused frame or evict a page from some frame. Suppose this frame number is f1
+        //      Write 0 in all of its contents (only necessary if next layer is a table)
+        //      PMwrite(0 + 5, f1)
+        //      addr1 = f1
+        //      PMread(addr1 * PAGE_SIZE + 1, &addr2) // second translation
+        //      If (addr2 == 0) then
+        //      Find an unused frame or evict a page from some frame. Suppose this frame number is f2.
+        //      Make sure you are not evicting f1 by accident
+        //      Restore the page we are looking for to frame f2 (only necessary pointing to a page)
+        //      PMwrite(addr1 * PAGE_SIZE + 1, f2)
+        //      addr2 = f2
+        //      PMwrite(addr2 * PAGE_SIZE + 6, value)
+        //where to use offset?
+        uint64_t currentBit = (pageNum >> i) & 1;
+        sum += currentBit;
+        word_t tempValue = 0;//
+        //sholud check both options of the offset? (or even general number of
+        // the offset)
+        PMread(sum, &tempValue);
+        if (tempValue != 0)
         {
-          sum += 1;
-          physicalAddress = (sum << OFFSET_WIDTH) | currentBit;
-          PMwrite (physicalAddress, 0);
+            sum += 1;
+            physicalAddress = (sum << OFFSET_WIDTH) | currentBit;
+            PMwrite(physicalAddress, 0);
         }
     }
-  PMrestore (sum,pageNum);//unused frame index?
-  uint64_t addressToWriteTo= (sum << OFFSET_WIDTH) | offset;
-  PMwrite (addressToWriteTo, value);
-
-
-
-
-
+    PMrestore(sum, pageNum);//unused frame index?
+    uint64_t addressToWriteTo = (sum << OFFSET_WIDTH) | offset;
+    PMwrite(addressToWriteTo, value);
 }
 
 
