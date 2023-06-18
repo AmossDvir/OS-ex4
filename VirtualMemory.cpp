f2f76ed759d3dda5d9d1430353abcae18b2ea17d
AmossDvir1 <dvir.amoss@gmail.com>
2023-06-18 23:46:26 +0300

    starting to implement the DFS algo

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -31,6 +31,16 @@ uint64_t extractOffset(uint64_t virtualAddress)
     return virtualAddress & offsetMask;
 }
 
+
+void fillFrameWithZeros(uint64_t frameNumber)
+{
+    for (uint64_t i = 0; i < PAGE_SIZE; ++i)
+    {
+        PMwrite(PAGE_SIZE * frameNumber + i, 0);
+    }
+}
+
+
 /**
  * Validates the page number
  * @param pageNum
@@ -41,6 +51,70 @@ bool validatePageNumber(uint64_t pageNum)
     return (pageNum < VIRTUAL_MEMORY_SIZE);
 }
 
+
+
+void newFunction (word_t value)
+{
+    //iterate on every level of the tree - if it is the last one we reached
+    // a leaf
+    for (int level = 0; level < TABLES_DEPTH; level++)
+    {
+        //if this is the last layer
+        if (level == TABLES_DEPTH)
+        {
+            uint64_t parseAddressLast = 0;//todo
+            PMwrite (parseAddressLast, value);
+            break;
+        }
+
+        uint64_t tempAddress;
+        word_t valueAddress;
+        uint64_t parseAddress = 0;//todo: for example 101
+        uint64_t rootAddress = 0 * PAGE_SIZE;
+        uint64_t currentAddress = rootAddress + parseAddress;
+        PMread (currentAddress, &valueAddress);
+        if (valueAddress == 0)
+        {
+            /* find unused frame or evict*/
+            word_t tempFrame = 0;//f1 todo check the type
+            if (level < TABLES_DEPTH - 1)
+            {//todo check what the fit condition - do it
+                // only if the next layer is a table
+                fillFrameWithZeros (valueAddress);
+            }
+            PMwrite (currentAddress, tempFrame);
+            tempAddress = tempFrame;
+        }
+    }
+
+}
+
+
+
+/**
+ *
+ */                                                      //      000000000000   1ULL << (treeLevel * NUM_PAGES)
+void traverseThroughTable(uint64_t page, int treeLevel, uint64_t baseAddress) //dfs  100110011001  [100][110][011][001]
+{
+//    Base cond:
+    if (treeLevel >= TABLES_DEPTH){
+        return;
+    }
+
+    uint64_t lastBits = (1ULL << (treeLevel * NUM_PAGES)) & page;
+    baseAddress = baseAddress + lastBits;
+    if (baseAddress == 0){
+        //Creates new frame:
+
+    }
+
+    word_t newAddr;
+    PMread(baseAddress, &newAddr);
+
+    traverseThroughTable(page, treeLevel + 1, baseAddress)
+
+}
+
 /**
  * Translates virtual address to a physical address
  * @param virtualAddress
@@ -49,16 +123,15 @@ bool validatePageNumber(uint64_t pageNum)
  */
 int translateLogicToPhysical(uint64_t virtualAddress, uint32_t *physical_address)
 {
+    // [011101001][010]
     uint64_t offset = extractOffset(virtualAddress);
-    uint64_t pageNum = extractPageNumber(virtualAddress);
+    uint64_t pageNum = extractPageNumber(virtualAddress); // p1[011]p2[101]p3[001]
     if (!validatePageNumber(pageNum))
     {
         return FAILURE;
     }
-
-    //go to the page tables
-    uint64_t frameNum = 0; //pages table[pageNum]
-    *physical_address = (frameNum << OFFSET_WIDTH) | offset;
+    traverseThroughTable(pageNum, 0, 0, );
+//    *physical_address = (frameNum << OFFSET_WIDTH) | offset;
     return SUCCESS;
 }
 
@@ -114,83 +187,6 @@ int VMwrite(uint64_t virtualAddress, word_t value)
 }
 
 
-bool isPageInPhysical(uint64_t pageNum)
-{
-    return false;//page table [page][isValid]
-}
-
-void moveToHardDisk()
-{
-}
-
-//layersNum=TABLES_DEPTH
-void tryFunc(uint64_t virtualAddress, word_t value)
-{
-    uint64_t frameNum = 0;
-    uint64_t tempOffset = 0;
-
-    uint64_t offset = virtualAddress & offsetMask;//1
-    uint64_t pageNum = virtualAddress & pageMask;//0110(6)
-
-    //  uint64_t unusedFrame=0;
-    uint64_t sum = 0;
-    uint64_t physicalAddress = 0;
-    for (int i = 0; i < TABLES_DEPTH; i++)
-    {
-        //      PMread(0 + 5, &addr1) // first translation
-        //      If (addr1 == 0) then
-        //      Find an unused frame or evict a page from some frame. Suppose this frame number is f1
-        //      Write 0 in all of its contents (only necessary if next layer is a table)
-        //      PMwrite(0 + 5, f1)
-        //      addr1 = f1
-        //      PMread(addr1 * PAGE_SIZE + 1, &addr2) // second translation
-        //      If (addr2 == 0) then
-        //      Find an unused frame or evict a page from some frame. Suppose this frame number is f2.
-        //      Make sure you are not evicting f1 by accident
-        //      Restore the page we are looking for to frame f2 (only necessary pointing to a page)
-        //      PMwrite(addr1 * PAGE_SIZE + 1, f2)
-        //      addr2 = f2
-        //      PMwrite(addr2 * PAGE_SIZE + 6, value)
-        //where to use offset?
-        uint64_t currentBit = (pageNum >> i) & 1;
-        sum += currentBit;
-        word_t tempValue = 0;//
-        //sholud check both options of the offset? (or even general number of
-        // the offset)
-        PMread(sum, &tempValue);
-        if (tempValue != 0)
-        {
-            sum += 1;
-            physicalAddress = (sum << OFFSET_WIDTH) | currentBit;
-            PMwrite(physicalAddress, 0);
-        }
-    }
-    PMrestore(sum, pageNum);//unused frame index?
-    uint64_t addressToWriteTo = (sum << OFFSET_WIDTH) | offset;
-    PMwrite(addressToWriteTo, value);
-}
-
-
-//go on the tree according to the levels
-//according to the address move right and left on the tree
-//if ==0
-
-//PMread(0 + 5, &addr1) // first translation
-//If (addr1 == 0) then
-//    Find an unused frame or evict a page from some frame. Suppose this frame number is f1
-//    Write 0 in all of its contents (only necessary if next layer is a table)
-//PMwrite(0 + 5, f1)
-//addr1 = f1
-//PMread(addr1 * PAGE_SIZE + 1, &addr2) // second translation
-//If (addr2 == 0) then
-//    Find an unused frame or evict a page from some frame. Suppose this frame number is f2.
-//Make sure you are not evicting f1 by accident
-//Restore the page we are looking for to frame f2 (only necessary pointing to a page)
-//PMwrite(addr1 * PAGE_SIZE + 1, f2)
-//addr2 = f2
-//PMwrite(addr2 * PAGE_SIZE + 6, value)
-
-
 
 
 /*** paging:
@@ -219,6 +215,4 @@ mmu -> divide to page number & offset (p|d)
     *when was the last access to page-timestamp
     * whan a page we want to access not mapped to a frame(valid bit =0)
     * there is page fault
-
-
 */
